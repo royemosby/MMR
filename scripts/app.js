b89df98a779af888d10f6a5b1190957cc0740c6b
Roy Mosby <ego.madking@gmail.com>
2019-03-28 05:51:52 -0400

    removed footer menu event listeners

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,5 @@
 const topMenu = document.querySelector('.header-menu');
 const topWork = topMenu.querySelector('[data-work]');
-const bottomMenu = document.querySelector('.footer-menu');
-const bottomWork = bottomMenu.querySelector('[data-work]');
 
 topWork.addEventListener('mouseover', () => {
   const topWorkSubMenu = topMenu.querySelector('.work-submenu');
@@ -10,21 +8,9 @@ topWork.addEventListener('mouseover', () => {
 
 topWork.addEventListener('mouseout', () => {
   const topWorkSubMenu = topMenu.querySelector('.work-submenu');
-  setTimeout(function () {
+  setTimeout(() => {
     topWorkSubMenu.classList.add('is-hidden');
   }, (1000));
 });
 
-bottomWork.addEventListener('mouseover', () => {
-  const bottomWorkSubMenu = bottomMenu.querySelector('.work-submenu');
-  bottomWorkSubMenu.classList.remove('is-hidden');
-});
-
-bottomWork.addEventListener('mouseout', () => {
-  const bottomWorkSubMenu = bottomMenu.querySelector('.work-submenu');
-  setTimeout(function () {
-    bottomWorkSubMenu.classList.add('is-hidden');
-  }, (1000));
-});
-
-// need to deal with fingers and onclicks
+// TODO need to deal with fingers and onclicks
