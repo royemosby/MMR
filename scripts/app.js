6b0afde502d30a3728a980f5653a718062783ad7
Roy Mosby <ego.madking@gmail.com>
2019-04-05 17:35:52 -0400

    removed sub-menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,6 @@
-const topMenu = document.querySelector('.header-menu');
-const topWork = topMenu.querySelector('[data-work]');
 
-topWork.addEventListener('mouseover', () => {
-  const topWorkSubMenu = topMenu.querySelector('.work-submenu');
-  topWorkSubMenu.classList.remove('is-hidden');
-});
 
-topWork.addEventListener('mouseout', () => {
-  const topWorkSubMenu = topMenu.querySelector('.work-submenu');
-  setTimeout(() => {
-    topWorkSubMenu.classList.add('is-hidden');
-  }, (1000));
-});
-
-// TODO need to deal with fingers and onclicks
+// TODO: need to deal with fingers and onclicks
+// [ ] top menu
+// [ ] project cards
+// TODO: easer egg
