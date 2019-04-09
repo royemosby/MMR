58bb5ad83dc87377ed0dd6a7d5174b3cbfeefa9e
Roy Mosby <ego.madking@gmail.com>
2019-04-09 04:06:53 -0400

    classList.toggle on .details to to bypass :hover issues on mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
+const details = document.getElementsByClassName('details');
 
+for (let i = 0; i < details.length; i++) {
+  details[i].addEventListener('click', e => {
+    details[i].classList.toggle('is-active');
+    //console.log(e, e.path.includes('div.details'));
+    if (details[i].classList.contains('is-active') === false) {
+      details[i].blur();
+    }
+  });
+}
 
 // TODO: need to deal with fingers and onclicks
 // [ ] top menu
 // [ ] project cards
-// TODO: easer egg
+// TODO: easter egg
