e587f2dc3194b7bf9ad6b7f366bd2ac16d8f3f59
Roy Mosby <ego.madking@gmail.com>
2019-06-30 17:12:16 -0400

    rmv .side-card: not needed. Changed #campaigns, #websites to #projects

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,4 @@
-const details = document.getElementsByClassName('details');
 
-for (let i = 0; i < details.length; i++) {
-  details[i].addEventListener('click', e => {
-    details[i].classList.toggle('is-active');
-    //console.log(e, e.path.includes('div.details'));
-    if (details[i].classList.contains('is-active') === false) {
-      details[i].blur();
-    }
-  });
-}
 
 // TODO: need to deal with fingers and onclicks
 // [ ] top menu
