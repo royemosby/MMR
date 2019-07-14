fa4ea3d1f6086fcd9e18d1cc4c874b9daaac7e8d
Roy Mosby <ego.madking@gmail.com>
2019-07-14 18:11:54 -0400

    pdf manual passthru

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('images');
-  //eleventyConfig.addPassthroughCopy('styles');
+  eleventyConfig.addPassthroughCopy('styles');
   eleventyConfig.addPassthroughCopy('scripts');
+  eleventyConfig.addPassthroughCopy('Maggie_Lynn_Resume_2019.pdf');
 
   return {
     dir: {
       includes: 'includes',
     },
-    templateFormats: ['css', 'js', 'map', 'liquid', 'md', 'html', 'pdf'],
+    templateFormats: ['js', 'map', 'liquid', 'md', 'html'],
     passthroughFileCopy: true,
   };
 };
